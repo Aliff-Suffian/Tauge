2da9f905b671ce9644d09f5d60c5c948822dd1b5
Aliff <1211201920@student.mmu.edu.my>
2023-08-15 08:59:57 +0800

    Add registration & login

---
+++ Tauge.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require 'rubygems'
+
+configure do
+  enable :sessions
+end
+
+get '/' do
+ '<a href="/login">Sign In</a>' '<a href="/getstarted">Get Started</a>'
+end
+
+get '/getstarted' do
+  session[:identify]? session.delete(:identify) : ''
+  '<form action="/login" method="POST">
+  Name : <input type="text" name="identify">
+  <br>Password : <input type="text">
+  <br>Confirm Password : <input type="text">
+  <br><button>Sign Up</button>
+  </form>'
+end
+
+get '/login' do
+  session[:identify]? session.delete(:identify) : ''
+  '<form action="/login" method="POST">
+  Name : <input type="text" name="identify">
+  <br>Password : <input type="text">
+  <br><button>Sign In</button>
+  </form>'
+end
+
+post '/getstarted' do
+  session[:identity] = params['identity']
+	redirect to '/'
+end
+
+post '/login' do
+  session[:identity] = params['identity']
+	redirect to '/'
+end
