c2fccdea18abfd37dbe14c7a20284b882aeb10ed
Aliff <1211201920@student.mmu.edu.my>
2023-08-17 15:37:34 +0800

    Update Database

--- config/app.rb
+++ config/app.rb
@@ -3,14 +3,15 @@ require 'sinatra/activerecord'
 require './config/users.rb'
 
 set :database, "sqlite3:tauge.sqlite3"
-set :public_folder, __dir__ + '/static'
+set :public_folder, File.expand_path('../../static', __FILE__)
+set :views, File.expand_path('../../views', __FILE__)
 
 configure do
   enable :sessions
 end
 
 get '/' do
-
+  @users = User.all
   erb :index
 end
 
@@ -38,5 +39,10 @@ end
 
 get '/dashboard' do
     erb :dashboard
+end
+
+get '/' do
+	@posts = User.all
+	erb :index
 end
 
